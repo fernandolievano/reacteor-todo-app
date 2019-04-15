166ccf6bdd22c6c6013723d19abaed0fd81ecdeb
fernandolievano <fernandolievan@gmail.con>
2019-04-15 01:23:52 -0300

    todo app react/meteor

---
+++ imports/ui/App.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Tasks } from '../api/tasks';
+import Task from './Task.jsx';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hideCompleted: false
+    }
+  }
+
+  renderTasks() {
+    let filteredTasks = this.props.tasks
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked)
+    }
+    return filteredTasks.map((task) => (
+      <Task key={ task._id } task={ task } />
+    ))
+  }
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted
+    })
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim()
+
+    Tasks.insert({
+      text,
+      createdAt: new Date()
+    })
+
+    ReactDOM.findDOMNode(this.refs.textInput).value = ''
+  }
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List ({ this.props.incompleteCount })</h1>
+        </header>
+
+        <label className="hide-completed">
+          <input 
+            type="checkbox" 
+            readOnly
+            checked={this.state.hideCompleted}
+            onClick={this.toggleHideCompleted.bind(this)}
+          />
+          Hide Completed Tasks
+        </label>
+
+        <ul>
+          { this.renderTasks() }
+        </ul>
+        
+        <form 
+          className="new-task"
+          onSubmit={this.handleSubmit.bind(this)}
+        >
+          <input 
+            type="text"
+            ref="textInput"
+            placeholder="Type to add new tasks"
+          />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default withTracker(() => {
+  return {
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count()
+  }
+})(App);
